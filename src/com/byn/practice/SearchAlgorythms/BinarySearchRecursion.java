09e0c1337a058e677fe6cdabe67fccda24e66741
Yuriy Bolotnyy <yuriy.bolotnyy@gmail.com>
2016-02-10 19:30:32 -0800

    binary search plain and recursion

--- src/com/byn/practice/SearchAlgorythms/BinarySearchRecursion.java
+++ src/com/byn/practice/SearchAlgorythms/BinarySearchRecursion.java
@@ -1,13 +1,15 @@
 package com.byn.practice.SearchAlgorythms;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by BYN on 2/9/16.
  */
-public class BinarySearch {
+public class BinarySearchRecursion {
     public static void main(String[] args) {
-        int[] validArr = {11, 33, 55, 77, 99, 111, 333, 555, 777, 999};
+        int[] validArr = {11, 33, 55, 59, 77, 99, 111, 333, 555, 777, 999};
         int[] emptyArr = {};
         int[] oneElementArr = {555};
 
@@ -18,10 +20,10 @@ public class BinarySearch {
 
         int[] arr = arrayMap.get("validArray");
         System.out.printf("Searching element %d in '%s' \n", 77, "validArray");
-        System.out.println("position: " + binarySearch(arr,77));
+        System.out.println("position: " + binarySearchReccur(arr,77, 0, arr.length));
     }
 
-    public static int binarySearch(int[] arr, int key){
+    public static int binarySearchReccur(int[] arr, int key, int leftBound, int rightBound){
         System.out.println(Arrays.toString(arr));
         if (arr.length == 0 || arr == null) {
             return -100; // empty array
@@ -35,13 +37,10 @@ public class BinarySearch {
 
         System.out.printf("Searching for '%d' value \n\n", key);
 
-        int lefBound = 0;
-        int rightBound = arr.length-1;
-
-        while (lefBound <= rightBound){
-            int currentPointer = lefBound + (rightBound - lefBound) / 2;
+        while (leftBound <= rightBound){
+            int currentPointer = leftBound + (rightBound - leftBound) / 2;
             System.out.printf("leftBound=%d, currentPointer=%d, rightBound=%d   \n\n",
-                    lefBound, currentPointer, rightBound);
+                    leftBound, currentPointer, rightBound);
 
             if (arr[currentPointer] == key){
                 System.out.printf("Found key:%d in arr[%d]! \n", key, currentPointer);
@@ -49,11 +48,11 @@ public class BinarySearch {
             } else if (key < arr[currentPointer]){
                 // go left
                 System.out.printf("key:%d < arr[%d]:%d, so going left \n", key, currentPointer, arr[currentPointer]);
-                rightBound = currentPointer - 1;
+                return binarySearchReccur(arr, key, leftBound, currentPointer - 1);
             } else {
                 // go right
                 System.out.printf("key:%d >= arr[%d]:%d, so going right \n", key, currentPointer, arr[currentPointer]);
-                lefBound = currentPointer + 1;
+                return binarySearchReccur(arr, key, currentPointer + 1, rightBound);
             }
         }
         return -1;
