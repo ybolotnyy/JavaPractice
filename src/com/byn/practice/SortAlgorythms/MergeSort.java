af939993a001bb0d746603f765385a75518775b5
Yuriy Bolotnyy <yuriy.bolotnyy@gmail.com>
2016-02-17 12:28:03 -0800

    mergeSort works

--- src/com/byn/practice/SortAlgorythms/MergeSort.java
+++ src/com/byn/practice/SortAlgorythms/MergeSort.java
@@ -8,24 +8,22 @@ import java.util.Arrays;
 public class MergeSort {
 
   public static void main(String[] args){
-    int[] arr = {38,27,43,3,9,82,10};
+    int[] arr = {9, 7, 5, 3, 1, 2, 4, 6, 8, 0};
     System.out.println(Arrays.toString(arr));
-    System.out.println(Arrays.toString(mergeSort(arr, 0, arr.length - 1)));
+    mergeSort(arr, 0, arr.length - 1);
+    System.out.println(Arrays.toString(arr));
   }
 
-  public static int[] mergeSort(int[] arr, int left, int right){
+  public static void mergeSort(int[] arr, int left, int right){
     if (left < right){
-      int mid = left + (right - left) / 2; System.out.printf("left:%d, mid:%d, right:%d \n", left, mid, right);
-      mergeSort(arr, left, mid); System.out.printf("mergesort[%d-%d] \n", left, mid);
-      mergeSort(arr, mid + 1, right); System.out.printf("mergesort[%d-%d] \n", mid+1, right);
-      merge(arr, left, mid, right);System.out.printf("merge[%d,%d,%d] \n", left, mid, right);
-      System.out.println(Arrays.toString(arr));
-      System.out.println();
+      int mid = left + (right - left) / 2;
+      mergeSort(arr, left, mid);
+      mergeSort(arr, mid + 1, right);
+      merge(arr, left, mid, right);
     }
-    return arr;
   }
 
-  public static int[] merge(int[] arr, int left, int mid, int right){
+  public static void merge(int[] arr, int left, int mid, int right){
     int[] temp = new int[arr.length];
     for (int i = 0; i < arr.length; i++) {
       temp[i] = arr[i];
@@ -46,18 +44,10 @@ public class MergeSort {
       k++;
     }
 
-    if (i <= mid) {
+    while (i <= mid) {
       arr[k] = temp[i];
       k++;
       i++;
     }
-
-    if (j <= right){
-      arr[k] = temp[i];
-      k++;
-      i++;
-    }
-
-    return arr;
   }
 }
