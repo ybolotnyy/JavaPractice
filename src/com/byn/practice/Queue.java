cbfbbd41c39f959b7766d4a6ee544f6e7ea298f8
Yuriy Bolotnyy <yuriy.bolotnyy@gmail.com>
2016-02-23 22:03:55 -0800

    queue refactored

--- src/com/byn/practice/Queue.java
+++ src/com/byn/practice/Queue.java
@@ -33,8 +33,8 @@ public class Queue {
 
   public Queue(int s) {
     this.maxSize = s;
-    this.nItems = -1;
-    this.front = -1;
+    this.nItems = 0;
+    this.front = 0;
     this.rear = -1;
     this.queValues = new int[maxSize];
     System.out.println("Initialized queue array length " + queValues.length + ": " + Arrays.toString(queValues));
@@ -49,9 +49,9 @@ public class Queue {
 
   public int remove() {
     if (! isEmpty()) {
-      int value = queValues[++front];
+      int value = queValues[front++];
       nItems--;
-      System.out.printf("Removed %d - ", value);
+      System.out.printf("Removed %d. \n ", value);
       return value;
     }
     return -100;
@@ -74,11 +74,42 @@ public class Queue {
       System.out.println("Can't peek. Queue is empty");
       return -1000;
     }
-    System.out.printf("Peeked from front: %d", queValues[front]);
+    System.out.printf("Peeked from front: %d \n", queValues[front]);
     return queValues[front];
   }
 
   public void print() {
+
+    System.out.printf("%d elements in queue: ", nItems);
+    System.out.print("[");
+    for (int i = 0; i < queValues.length; i++) {
+      if (i == front) {
+        System.out.print("<");
+      }
+
+      System.out.print(queValues[i]);
+
+      if (i == rear) {
+        System.out.print("<");
+      }
+
+      if (i < queValues.length - 1) {
+        System.out.print(", ");
+      }
+    }
+    System.out.print("]");
+
+    if (isEmpty()) {
+      System.out.printf("  Queue is empty!");
+    } else
+    if (isFull()) {
+      System.out.printf("  Queue is full!");
+    }
+
+    System.out.println();
+  }
+
+  public void printr() {
 
     final String COMMA = ", ";
     final String FRONT_MARKER = " >>";
@@ -89,15 +120,15 @@ public class Queue {
     for (int i = maxSize - 1; i >= 0; i--) {
     //  if (queValues[i] == )
       if (i == 0) {
-        System.out.printf("%s", queValues[i]);
+        System.out.printf("%d", queValues[i]);
       } else if (i == maxSize - 1) {
-        System.out.printf("%s%s", queValues[i], COMMA);
+        System.out.printf("%d%s", queValues[i], COMMA);
       } else if (i == front) {
-        System.out.printf("%s%s%s", FRONT_MARKER, queValues[i]);
+        System.out.printf("%s%s%d", FRONT_MARKER, queValues[i]);
       } else if (i == rear) {
-        System.out.printf("%s%s", queValues[i], REAR_MARKER);
+        System.out.printf("%d%s", queValues[i], REAR_MARKER);
       } else
-      System.out.printf("%s%s", queValues[i], COMMA);
+      System.out.printf("%d%s", queValues[i], COMMA);
     }
     System.out.print("]");
 
